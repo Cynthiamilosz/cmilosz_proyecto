2cf705edb5b88f5be66088f74a2ffcca859207f0
Cynthia Milosz <cynthia.milosz@gmail.com>
2022-10-24 02:43:11 -0300

    versionveintiocho

---
+++ dataconsulta.js
@@ -0,0 +1,12 @@
+//function addConsulta (pnombre, papellido, pmail, pcomentario){
+
+    //let newConsulta = {
+        
+       // nombre : pnombre,
+       // apellido : papellido,
+       // mail : pmail,
+      //  comentario : pcomentario
+   // };
+    
+   // consultasList.push(newConsulta);
+   // }
